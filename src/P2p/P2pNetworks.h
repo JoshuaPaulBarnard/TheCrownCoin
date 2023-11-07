bf36e7f00c45f8a45a2ebf675498c2af7aa2c9bf
Joshua Paul Barnard <joshuapaulbarnard@gmail.com>
2023-11-07 15:12:37 -0800

    revision - nov7

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x25, 0xe3, 0xdd, 0x43, 0x14, 0x14, 0xdd, 0xd7, 0x14, 0xfb, 0xe, 0xe, 0xcb, 0x31, 0x37, 0x25 } };
+}
